986463f883f48644fbc711188d7f7444c13fd0f1
Vikum <vikumkumarawadu2@gmail.com>
2025-12-02 13:43:15 +1300

    Implemented a chat history

--- chatbot/chatbot/build/app.js
+++ chatbot/chatbot/build/app.js
@@ -1,5 +1,6 @@
 function _regenerator() { /*! regenerator-runtime -- Copyright (c) 2014-present, Facebook, Inc. -- license (MIT): https://github.com/babel/babel/blob/main/packages/babel-helpers/LICENSE */ var e, t, r = "function" == typeof Symbol ? Symbol : {}, n = r.iterator || "@@iterator", o = r.toStringTag || "@@toStringTag"; function i(r, n, o, i) { var c = n && n.prototype instanceof Generator ? n : Generator, u = Object.create(c.prototype); return _regeneratorDefine2(u, "_invoke", function (r, n, o) { var i, c, u, f = 0, p = o || [], y = !1, G = { p: 0, n: 0, v: e, a: d, f: d.bind(e, 4), d: function d(t, r) { return i = t, c = 0, u = e, G.n = r, a; } }; function d(r, n) { for (c = r, u = n, t = 0; !y && f && !o && t < p.length; t++) { var o, i = p[t], d = G.p, l = i[2]; r > 3 ? (o = l === n) && (u = i[(c = i[4]) ? 5 : (c = 3, 3)], i[4] = i[5] = e) : i[0] <= d && ((o = r < 2 && d < i[1]) ? (c = 0, G.v = n, G.n = i[1]) : d < l && (o = r < 3 || i[0] > n || n > l) && (i[4] = r, i[5] = n, G.n = l, c = 0)); } if (o || r > 1) return a; throw y = !0, n; } return function (o, p, l) { if (f > 1) throw TypeError("Generator is already running"); for (y && 1 === p && d(p, l), c = p, u = l; (t = c < 2 ? e : u) || !y;) { i || (c ? c < 3 ? (c > 1 && (G.n = -1), d(c, u)) : G.n = u : G.v = u); try { if (f = 2, i) { if (c || (o = "next"), t = i[o]) { if (!(t = t.call(i, u))) throw TypeError("iterator result is not an object"); if (!t.done) return t; u = t.value, c < 2 && (c = 0); } else 1 === c && (t = i["return"]) && t.call(i), c < 2 && (u = TypeError("The iterator does not provide a '" + o + "' method"), c = 1); i = e; } else if ((t = (y = G.n < 0) ? u : r.call(n, G)) !== a) break; } catch (t) { i = e, c = 1, u = t; } finally { f = 1; } } return { value: t, done: y }; }; }(r, o, i), !0), u; } var a = {}; function Generator() {} function GeneratorFunction() {} function GeneratorFunctionPrototype() {} t = Object.getPrototypeOf; var c = [][n] ? t(t([][n]())) : (_regeneratorDefine2(t = {}, n, function () { return this; }), t), u = GeneratorFunctionPrototype.prototype = Generator.prototype = Object.create(c); function f(e) { return Object.setPrototypeOf ? Object.setPrototypeOf(e, GeneratorFunctionPrototype) : (e.__proto__ = GeneratorFunctionPrototype, _regeneratorDefine2(e, o, "GeneratorFunction")), e.prototype = Object.create(u), e; } return GeneratorFunction.prototype = GeneratorFunctionPrototype, _regeneratorDefine2(u, "constructor", GeneratorFunctionPrototype), _regeneratorDefine2(GeneratorFunctionPrototype, "constructor", GeneratorFunction), GeneratorFunction.displayName = "GeneratorFunction", _regeneratorDefine2(GeneratorFunctionPrototype, o, "GeneratorFunction"), _regeneratorDefine2(u), _regeneratorDefine2(u, o, "Generator"), _regeneratorDefine2(u, n, function () { return this; }), _regeneratorDefine2(u, "toString", function () { return "[object Generator]"; }), (_regenerator = function _regenerator() { return { w: i, m: f }; })(); }
 function _regeneratorDefine2(e, r, n, t) { var i = Object.defineProperty; try { i({}, "", {}); } catch (e) { i = 0; } _regeneratorDefine2 = function _regeneratorDefine(e, r, n, t) { function o(r, n) { _regeneratorDefine2(e, r, function (e) { return this._invoke(r, n, e); }); } r ? i ? i(e, r, { value: n, enumerable: !t, configurable: !t, writable: !t }) : e[r] = n : (o("next", 0), o("throw", 1), o("return", 2)); }, _regeneratorDefine2(e, r, n, t); }
+function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t["return"] || t["return"](); } finally { if (u) throw o; } } }; }
 function asyncGeneratorStep(n, t, e, r, o, a, c) { try { var i = n[a](c), u = i.value; } catch (n) { return void e(n); } i.done ? t(u) : Promise.resolve(u).then(r, o); }
 function _asyncToGenerator(n) { return function () { var t = this, e = arguments; return new Promise(function (r, o) { var a = n.apply(t, e); function _next(n) { asyncGeneratorStep(a, r, o, _next, _throw, "next", n); } function _throw(n) { asyncGeneratorStep(a, r, o, _next, _throw, "throw", n); } _next(void 0); }); }; }
 function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
@@ -14,12 +15,12 @@ import { Router, Routes, Route } from "@jac-client/utils";
 function Chat() {
   var _useState = useState(""),
     _useState2 = _slicedToArray(_useState, 2),
-    message = _useState2[0],
-    setMessage = _useState2[1];
-  var _useState3 = useState(""),
+    currentMessage = _useState2[0],
+    setCurrentMessage = _useState2[1];
+  var _useState3 = useState([]),
     _useState4 = _slicedToArray(_useState3, 2),
-    reply = _useState4[0],
-    setReply = _useState4[1];
+    chatHistory = _useState4[0],
+    setChatHistory = _useState4[1];
   var _useState5 = useState(false),
     _useState6 = _slicedToArray(_useState5, 2),
     loading = _useState6[0],
@@ -29,7 +30,7 @@ function Chat() {
   }
   function _sendMessage() {
     _sendMessage = _asyncToGenerator(/*#__PURE__*/_regenerator().m(function _callee() {
-      var user_msg, result, reply_text, r;
+      var user_msg, formatted_history, _iterator, _step, msg, userMessageObj, result, reply_text, r, botMessageObj;
       return _regenerator().w(function (_context) {
         while (1) switch (_context.p = _context.n) {
           case 0:
@@ -39,21 +40,52 @@ function Chat() {
             }
             return _context.a(2);
           case 1:
-            if (message.trim()) {
+            if (currentMessage.trim()) {
               _context.n = 2;
               break;
             }
             return _context.a(2);
           case 2:
-            user_msg = message.trim();
+            user_msg = currentMessage.trim();
+            formatted_history = "";
+            if (chatHistory.length > 0) {
+              formatted_history = "Previous conversation:\\n";
+              _iterator = _createForOfIteratorHelper(chatHistory);
+              try {
+                for (_iterator.s(); !(_step = _iterator.n()).done;) {
+                  msg = _step.value;
+                  if (msg.sender === "user") {
+                    formatted_history += "User: " + msg.text + "\\n";
+                  } else {
+                    formatted_history += "Bot: " + msg.text + "\\n";
+                  }
+                }
+              } catch (err) {
+                _iterator.e(err);
+              } finally {
+                _iterator.f();
+              }
+            }
+            console.log("formatted_history:", formatted_history);
+            userMessageObj = {
+              "text": user_msg,
+              "sender": "user",
+              "timestamp": Date.now()
+            };
+            setChatHistory(function (prev) {
+              return prev.concat([userMessageObj]);
+            });
+            setCurrentMessage("");
             setLoading(true);
             _context.p = 3;
             _context.n = 4;
             return __jacSpawn("chat_agent", "", {
-              "message": message
+              "userinput": user_msg,
+              "formatted_history": formatted_history
             });
           case 4:
             result = _context.v;
+            console.log("LLM result:", result);
             reply_text = "";
             if (result && result.reports && result.reports.length > 0) {
               r = result.reports[0];
@@ -63,7 +95,14 @@ function Chat() {
                 reply_text = "" + r;
               }
             }
-            setReply(reply_text);
+            botMessageObj = {
+              "text": reply_text,
+              "sender": "bot",
+              "timestamp": Date.now()
+            };
+            setChatHistory(function (prev) {
+              return prev.concat([botMessageObj]);
+            });
           case 5:
             _context.p = 5;
             setLoading(false);
@@ -90,10 +129,27 @@ function Chat() {
       "color": "#666",
       "marginBottom": "12px"
     }
-  }, ["Type a question"]), __jacJsx("textarea", {
-    "value": message,
+  }, ["Type a question"]), __jacJsx("div", {
+    "style": {
+      "height": "400px",
+      "maxHeight": "400px",
+      "overflowY": "auto",
+      "border": "1px solid #e5e7eb",
+      "borderRadius": "8px",
+      "padding": "16px",
+      "marginBottom": "16px",
+      "backgroundColor": "#f9fafb"
+    }
+  }, [__jacJsx("div", {
+    "style": {
+      "color": "#9ca3af",
+      "textAlign": "center",
+      "padding": "40px 0"
+    }
+  }, ["Chat history will appear here..."])]), __jacJsx("textarea", {
+    "value": currentMessage,
     "onChange": function onChange(e) {
-      setMessage(e.target.value);
+      setCurrentMessage(e.target.value);
     },
     "onKeyPress": function onKeyPress(e) {
       if (e.key === "Enter" && e.shiftKey === false) {
@@ -117,26 +173,17 @@ function Chat() {
     }
   }, [__jacJsx("button", {
     "onClick": sendMessage,
-    "disabled": loading || !message.trim(),
+    "disabled": loading || !currentMessage.trim(),
     "style": {
       "padding": "10px 14px",
       "border": "none",
       "borderRadius": "6px",
-      "cursor": loading || !message.trim() ? "not-allowed" : "pointer",
-      "background": loading || !message.trim() ? "#9ca3af" : "#3b82f6",
+      "cursor": loading || !currentMessage.trim() ? "not-allowed" : "pointer",
+      "background": loading || !currentMessage.trim() ? "#9ca3af" : "#3b82f6",
       "color": "#fff",
       "fontWeight": "600"
     }
-  }, [loading ? "Sending\u2026" : "Send"])]), reply ? __jacJsx("div", {
-    "style": {
-      "marginTop": "14px",
-      "padding": "12px",
-      "background": "#f9fafb",
-      "border": "1px solid #e5e7eb",
-      "borderRadius": "6px",
-      "whiteSpace": "pre-wrap"
-    }
-  }, [reply]) : null]);
+  }, [loading ? "Sending\u2026" : "Send"])])]);
 }
 function app() {
   return __jacJsx(Router, {}, [__jacJsx(Routes, {}, [__jacJsx(Route, {
--- chatbot/chatbot/src/app.js
+++ chatbot/chatbot/src/app.js
@@ -2,20 +2,38 @@ import {__jacJsx, __jacSpawn} from "@jac-client/utils";
 import { useState } from "react";
 import { Router, Routes, Route } from "@jac-client/utils";
 function Chat() {
-  let [message, setMessage] = useState("");
-  let [reply, setReply] = useState("");
+  let [currentMessage, setCurrentMessage] = useState("");
+  let [chatHistory, setChatHistory] = useState([]);
   let [loading, setLoading] = useState(false);
   async function sendMessage() {
     if (loading) {
       return;
     }
-    if (!message.trim()) {
+    if (!currentMessage.trim()) {
       return;
     }
-    let user_msg = message.trim();
+    let user_msg = currentMessage.trim();
+    let formatted_history = "";
+    if (chatHistory.length > 0) {
+      formatted_history = "Previous conversation:\\n";
+      for (const msg of chatHistory) {
+        if (msg.sender === "user") {
+          formatted_history += "User: " + msg.text + "\\n";
+        } else {
+          formatted_history += "Bot: " + msg.text + "\\n";
+        }
+      }
+    }
+    console.log("formatted_history:", formatted_history);
+    let userMessageObj = {"text": user_msg, "sender": "user", "timestamp": Date.now()};
+    setChatHistory(prev => {
+      return prev.concat([userMessageObj]);
+    });
+    setCurrentMessage("");
     setLoading(true);
     try {
-      let result = await __jacSpawn("chat_agent", "", {"message": message});
+      let result = await __jacSpawn("chat_agent", "", {"userinput": user_msg, "formatted_history": formatted_history});
+      console.log("LLM result:", result);
       let reply_text = "";
       if (result && result.reports && result.reports.length > 0) {
         let r = result.reports[0];
@@ -25,19 +43,22 @@ function Chat() {
           reply_text = "" + r;
         }
       }
-      setReply(reply_text);
+      let botMessageObj = {"text": reply_text, "sender": "bot", "timestamp": Date.now()};
+      setChatHistory(prev => {
+        return prev.concat([botMessageObj]);
+      });
     } finally {
       setLoading(false);
     }
   }
-  return __jacJsx("div", {"style": {"maxWidth": "720px", "margin": "24px auto", "padding": "16px"}}, [__jacJsx("h2", {"style": {"marginBottom": "8px"}}, ["Chat Bot"]), __jacJsx("div", {"style": {"color": "#666", "marginBottom": "12px"}}, ["Type a question"]), __jacJsx("textarea", {"value": message, "onChange": e => {
-    setMessage(e.target.value);
+  return __jacJsx("div", {"style": {"maxWidth": "720px", "margin": "24px auto", "padding": "16px"}}, [__jacJsx("h2", {"style": {"marginBottom": "8px"}}, ["Chat Bot"]), __jacJsx("div", {"style": {"color": "#666", "marginBottom": "12px"}}, ["Type a question"]), __jacJsx("div", {"style": {"height": "400px", "maxHeight": "400px", "overflowY": "auto", "border": "1px solid #e5e7eb", "borderRadius": "8px", "padding": "16px", "marginBottom": "16px", "backgroundColor": "#f9fafb"}}, [__jacJsx("div", {"style": {"color": "#9ca3af", "textAlign": "center", "padding": "40px 0"}}, ["Chat history will appear here..."])]), __jacJsx("textarea", {"value": currentMessage, "onChange": e => {
+    setCurrentMessage(e.target.value);
   }, "onKeyPress": e => {
     if (e.key === "Enter" && e.shiftKey === false) {
       e.preventDefault();
       sendMessage();
     }
-  }, "rows": 5, "placeholder": "Ask me anything...", "style": {"width": "100%", "padding": "10px", "border": "1px solid #ddd", "borderRadius": "6px", "resize": "vertical", "boxSizing": "border-box"}}, []), __jacJsx("div", {"style": {"marginTop": "10px"}}, [__jacJsx("button", {"onClick": sendMessage, "disabled": loading || !message.trim(), "style": {"padding": "10px 14px", "border": "none", "borderRadius": "6px", "cursor": loading || !message.trim() ? "not-allowed" : "pointer", "background": loading || !message.trim() ? "#9ca3af" : "#3b82f6", "color": "#fff", "fontWeight": "600"}}, [loading ? "Sending\u2026" : "Send"])]), reply ? __jacJsx("div", {"style": {"marginTop": "14px", "padding": "12px", "background": "#f9fafb", "border": "1px solid #e5e7eb", "borderRadius": "6px", "whiteSpace": "pre-wrap"}}, [reply]) : null]);
+  }, "rows": 5, "placeholder": "Ask me anything...", "style": {"width": "100%", "padding": "10px", "border": "1px solid #ddd", "borderRadius": "6px", "resize": "vertical", "boxSizing": "border-box"}}, []), __jacJsx("div", {"style": {"marginTop": "10px"}}, [__jacJsx("button", {"onClick": sendMessage, "disabled": loading || !currentMessage.trim(), "style": {"padding": "10px 14px", "border": "none", "borderRadius": "6px", "cursor": loading || !currentMessage.trim() ? "not-allowed" : "pointer", "background": loading || !currentMessage.trim() ? "#9ca3af" : "#3b82f6", "color": "#fff", "fontWeight": "600"}}, [loading ? "Sending\u2026" : "Send"])])]);
 }
 function app() {
   return __jacJsx(Router, {}, [__jacJsx(Routes, {}, [__jacJsx(Route, {"path": "/", "element": __jacJsx(Chat, {}, [])}, [])])]);
